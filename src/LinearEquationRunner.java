4f50f734287ab8ad5c5ebc3b6051fdeb71ed3872
Dicky Chen <flamemyth21@gmail.com>
2022-10-17 18:00:09 -0400

    third commit (finished getting coordinate values)

--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -10,56 +10,22 @@ public class LinearEquationRunner {
         System.out.print("Enter coordinate 2: ");
         String co2 = scan.nextLine();
 
+        // coordinate value strings
+        String stringX1 = co1.substring(1, co1.indexOf(","));
+        String stringY1 = co1.substring(co1.indexOf(" ") + 1, co1.indexOf(")"));
+        String stringX2 = co2.substring(1, co2.indexOf(","));
+        String stringY2 = co2.substring(co2.indexOf(" ") + 1, co2.indexOf(")"));
+
         // checking for vertical line
-        if (co1.substring(1, 3).equals(co2.substring(1,3))) {
-            if (co1.substring(1,2).equals("-")) {
-                System.out.println("These coordinates result in a vertical line at x = " + co1.substring(1, 3));
-            } else {
-                System.out.println("These coordinates result in a vertical line at x = " + co1.substring(1, 2));
-            }
+        if (stringX1.equals(stringX2)) {
+            System.out.println("These coordinates result in a vertical line at x = " + stringX1);
         }
 
-        // coordinate values
-        int x1 = 0;
-        int x2 = 0;
-        int y1 = 0;
-        int y2 = 0;
-
-        // getting x1 value
-        if (co1.substring(1,2).equals("-")) {
-            x1 = Integer.parseInt(co1.substring(1,3));
-        } else {
-            x1 = Integer.parseInt(co1.substring(1, 2));
-        }
-
-        // getting x2 value
-        if (co2.substring(1,2).equals("-")) {
-            x2 = Integer.parseInt(co2.substring(1,3));
-        } else {
-            x2 = Integer.parseInt(co2.substring(1, 2));
-        }
-
-        // getting y1 value
-        if (co1.substring(5,6).equals("-")) {
-            y1 = Integer.parseInt(co1.substring(5, 7));
-        } else if (co1.substring(4, 5).equals("-")){
-            y1 = Integer.parseInt(co1.substring(4, 6));
-        } else if (co1.substring(3, 4).equals(",")) {
-            y1 = Integer.parseInt(co1.substring(5, 6));
-        } else {
-            y1 = Integer.parseInt(co1.substring(5, 6));
-        }
-
-        // getting y2 value
-        if (co2.substring(5,6).equals("-")) {
-            y2 = Integer.parseInt(co2.substring(5, 7));
-        } else if (co2.substring(4, 5).equals("-")){
-            y2 = Integer.parseInt(co2.substring(4, 6));
-        } else if (co2.substring(3, 4).equals(",")) {
-            y2 = Integer.parseInt(co2.substring(5, 6));
-        } else {
-            y2 = Integer.parseInt(co2.substring(4, 5));
-        }
+        // getting coordinate values
+        int x1 = Integer.parseInt(stringX1);
+        int y1 = Integer.parseInt(stringY1);
+        int x2 = Integer.parseInt(stringX2);
+        int y2 = Integer.parseInt(stringY2);
 
     }
 }
